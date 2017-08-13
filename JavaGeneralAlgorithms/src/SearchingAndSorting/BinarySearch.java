ac930052a5fc045e2cc5bfff527e2315b4ffcec7
Romil Chauhan <romilhc@gmail.com>
2017-08-13 00:34:46 -0400

    Binary Search Implemented

--- JavaGeneralAlgorithms/src/SearchingAndSorting/BinarySearch.java
+++ JavaGeneralAlgorithms/src/SearchingAndSorting/BinarySearch.java
@@ -15,21 +15,102 @@ public class BinarySearch {
     //Recursive binary search
     public int BinarySearchRecursive(int[] arr, int left, int right, int value) {
         
-        if(left < right) {
-            System.out.println("hello");
+        try {
+            
+            if(left <= right) {
+            
+                //Get middle element of the array 
+                int mid = left + (right - left)/2;
+
+                //If value is found
+                if(arr[mid] == value) {
+                    return mid;
+                }
+                //If value is on the left of middle element
+                else if(arr[mid] > value) {
+                    return BinarySearchRecursive(arr, left, mid - 1, value);
+                }
+                //If value is on the right of middle element
+                else if(arr[mid] < value) {
+                    return BinarySearchRecursive(arr, mid + 1, right, value);
+                }
+                
+            }
+            
+        }
+        catch(Exception ex) {
+            System.out.println(ex);
         }
         
+        //If element is not found
         return -1;
     }
+    
+    //Recursive binary search
+    public int BinarySearchIterative(int[] arr, int left, int right, int value) {
+        
+        try {
+            
+            while(left <= right) {
+            
+                //Get middle element of the array 
+                int mid = left + (right - left)/2;
+
+                //If value is found
+                if(arr[mid] == value) {
+                    return mid;
+                }
+                //If value is on the left of middle element
+                else if(arr[mid] > value) {
+                    right = mid - 1;
+                }
+                //If value is on the right of middle element
+                else if(arr[mid] < value) {
+                    left = mid + 1;
+                }
+            }
+        
+        }
+        catch(Exception ex) {
+            System.out.println(ex);
+        }
+     
+        //If element is not found
+        return -1;
+    }
+    
     
     //Main driver method
     public static void main(String args[]) {
         
-        int[] arr = {6, 50, 100, 101, 134, 411};
+        int[] arr = {6, 50, 100, 101, 134, 411, 600};
         int n = arr.length;
+        
         BinarySearch bs = new BinarySearch();
         
-        int resultIndex = bs.BinarySearchRecursive(arr,0,n-1,100);
+        //Testing recursive function
+        int resultIndex = bs.BinarySearchRecursive(arr,0,n-1,50);
+        if(resultIndex == -1) {
+            System.out.println("Value not found in an array");
+        }
+        else {
+            System.out.println("Value found at index: " + resultIndex);
+        }
+
+        //Testing iterative function        
+        resultIndex = bs.BinarySearchIterative(arr, 0, n-1, 134);
+        if(resultIndex == -1) {
+            System.out.println("Value not found in an array");
+        }
+        else {
+            System.out.println("Value found at index: " + resultIndex);
+        }
         
     }
 }
+
+/*
+OUTPUT:
+Value found at index: 1
+Value found at index: 4
+*/
