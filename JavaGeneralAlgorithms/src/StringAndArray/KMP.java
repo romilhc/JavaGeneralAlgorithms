5bb10297b3370f88265796870c8e0b5dc81a73b8
Romil Chauhan <romilhc@gmail.com>
2017-09-09 21:06:49 -0400

    Started working with KMP

---
+++ JavaGeneralAlgorithms/src/StringAndArray/KMP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StringAndArray;
+
+/**
+ *
+ * @author Romil Chauhan
+ */
+public class KMP {
+    
+    
+    public static void main(String args[]) {
+        
+        String s1 = "ABABDABACDABABCABAB";
+        String pattern = "ABABCABAB";
+        int LPS[] = new int[pattern.length()];
+        
+    }
+}
